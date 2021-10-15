98ffd5ed48729184ab043536ff75fe2ac254a007
yaldashbz <yaldashabanzadeh@gmail.com>
2021-10-15 21:19:00 +0330

    feat: add transfer method

--- src/scanner/dfa/state.py
+++ src/scanner/dfa/state.py
@@ -1,7 +1,7 @@
 from enum import Enum
 from typing import List
 
-from src.scanner.dfa import Transition
+from src.scanner.dfa import Transition, TransferException
 
 
 class StateType(Enum):
@@ -12,13 +12,15 @@ class StateType(Enum):
 
 class State(object):
 
-    def __init__(self, transitions: List[Transition], state_type: StateType):  # TODO
+    def __init__(self, transitions: List[Transition], state_type: StateType):
         self._transitions = transitions
         self.type = state_type
 
-    def transfer(self, character: str):  # -> TODO
-        # return state
-        pass
+    def transfer(self, character: str):
+        try:
+            return list(filter(lambda x: x.is_valid(character), self._transitions))[0]
+        except IndexError:
+            raise TransferException
 
-    def is_final(self):
+    def is_final(self) -> bool:
         return self.type == StateType.END
