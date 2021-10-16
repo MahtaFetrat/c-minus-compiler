a6d129593402907f98573154fabe4969470948fb
MahtaFetrat <77fetrat@gmail.com>
2021-10-16 10:21:48 +0330

    fix dfa builder tests

--- test/scanner/dfa/test_builder.py
+++ test/scanner/dfa/test_builder.py
@@ -37,17 +37,17 @@ class TestBuilder(unittest.TestCase):
 
     def test_build_dfa_start(self):
         dfa = build_dfa(dfa_dict)
-        self.assertEqual(State("0"), dfa.start_state)
+        self.assertEqual(State(0), dfa.start_state)
 
     def test_build_dfa_transitions(self):
         dfa = build_dfa(dfa_dict)
         transitions = dfa.start_state.transitions
-        self.assertIn(Transition(["*"], State("0")), transitions)
-        self.assertIn(Transition(["9"], State("1", True)), transitions)
+        self.assertIn(Transition(["*"], State(0)), transitions)
+        self.assertIn(Transition(["9"], State(1, True)), transitions)
 
         final_state_transitions = dfa.start_state.transitions[0].dest_state
         self.assertIn(
-            Transition(["*", "9"], State("1")), final_state_transitions.transitions
+            Transition(["*", "9"], State(1)), final_state_transitions.transitions
         )
 
 
