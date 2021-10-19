2af238e77ff2a8781a2a8a6e732d29f44ca30a50
MahtaFetrat <77fetrat@gmail.com>
2021-10-19 22:01:05 +0330

    fix test failure

--- test/scanner/dfa/test_builder.py
+++ test/scanner/dfa/test_builder.py
@@ -13,12 +13,12 @@ class TestBuilder(unittest.TestCase):
     def test_build_dfa_transitions(self):
         dfa = Builder(SIMPLE_DFA_DICT).build_dfa()
         transitions = dfa.start_state.transitions
-        self.assertIn(Transition(["*"], State(0)), transitions)
+        self.assertIn(Transition(["1"], State(0)), transitions)
         self.assertIn(Transition(["9"], State(1)), transitions)
 
         final_state_transitions = dfa.start_state.transitions[0].dest_state
         self.assertIn(
-            Transition(["*", "9"], State(1)), final_state_transitions.transitions
+            Transition(["1", "9"], State(1)), final_state_transitions.transitions
         )
 
 
