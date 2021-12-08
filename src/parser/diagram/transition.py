c1b05823357d80487762c4c3b2637b2b4ff4218b
MahtaFetrat <77fetrat@gmail.com>
2021-12-08 10:49:36 +0330

    change transition transfer input to token type

--- src/parser/diagram/transition.py
+++ src/parser/diagram/transition.py
@@ -1,14 +1,17 @@
 from src.base import Edge, List
 from src.parser.diagram.symbol import create_symbol
+from src.parser.utils import SymbolType
 
 
 class Transition(Edge):
-    def __init__(self, dest, name, predicts: List[str], symbol_type, diagram=None):
+    def __init__(
+        self, dest, name, predicts: List[str], symbol_type: SymbolType, diagram=None
+    ):
         self.symbol = create_symbol(name, symbol_type, predicts, diagram)
         super().__init__([self.symbol], dest)
 
     def accept(self, lookahead, scanner):
         return self.symbol.accept(lookahead, scanner), self.dest
 
-    def is_valid(self, character: str):
-        return self.symbol.handle(character)
+    def is_valid(self, lookahead):
+        return self.symbol.handle(lookahead)
